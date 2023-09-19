9b984f5d432e9913e3778bb0388c02018d545248
Hiro <laciferin@gmail.com>
2023-09-20 03:14:32 +0530

    add: dotenv
    Signed-off-by: Hiro <laciferin@gmail.com>

---
+++ web3-storage-quickstart/put-files.ts
@@ -0,0 +1,35 @@
+import process from 'process'
+import minimist from 'minimist'
+import { Web3Storage, getFilesFromPath } from 'web3.storage'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+let token = process.env.WEB3_API_KEY
+
+async function main () {
+  const args = minimist(process.argv.slice(2))
+  token = token ?? args.token
+
+  if (!token) {
+    return console.error('A token is needed. You can create one on https://web3.storage')
+  }
+
+  if (args._.length < 1) {
+    return console.error('Please supply the path to a file or directory')
+  }
+
+  const storage = new Web3Storage({ token })
+  const files = []
+
+  for (const path of args._) {
+    const pathFiles = await getFilesFromPath(path)
+    files.push(...pathFiles)
+  }
+
+  console.log(`Uploading ${files.length} files`)
+  const cid = await storage.put(files)
+  console.log('Content added with CID:', cid)
+}
+
+main()
