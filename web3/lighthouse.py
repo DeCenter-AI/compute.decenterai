fa7bf9894d6fdf81261b2d107347e5fc245e7e43
Nasfame <laciferin@gmail.com>
2023-09-25 04:15:05 +0530

    ligthous: upload and download
    Signed-off-by: Nasfame <laciferin@gmail.com>

--- web3/lighthouse.py
+++ web3/lighthouse.py
@@ -1,11 +1,38 @@
 from lighthouseweb3 import Lighthouse
 
 from dotenv import load_dotenv
+import os
+
+from dataclasses import dataclass
 
 
-lh = Lighthouse()
+@dataclass
+class UploadedFile:
+    name: str
+    hash: str
+    size: str
 
 
-if __name__=="__main__":
-  load_dotenv()
-  
+if __name__ == "__main__":
+    lh = Lighthouse(token='56c91764.0d69cc79074f460c86a9a6d0601a8f65')
+
+    response = lh.upload("__init__.py")
+    print(response)
+
+    data = response['data']
+
+    upFile = UploadedFile(data['Name'], data['Hash'], data['Size'])
+
+    if not os.path.exists('data'):
+        os.mkdir('data')
+        
+    sample_v3_cid = "QmP9xCDVx4N5uVNezeurdepMn9nrynpvuYVvVAZNPmYn1x"
+    path_to_save = "./data/x.zip"
+
+    with open(path_to_save, 'w') as f1:
+        res = lh.downloadBlob(f1.buffer, sample_v3_cid)
+        print(res)
+
+    # lh.download(sample_v3_cid) #TODO: just downloads in bytes
+
+    load_dotenv()
